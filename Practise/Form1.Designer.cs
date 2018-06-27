a3bcde35df5d13695837ed49a862a559de72924e
Данило В. Челишев <d.chelyshev@k503.d5.khai.edu>
2018-06-27 11:18:30 +0300

    Add project files.

---
+++ Practise/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace Practise
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxX1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxFreeCoef1 = new System.Windows.Forms.TextBox();
+            this.textBoxFreeCoef2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxX2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.buttonDraw = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBoxLine1 = new System.Windows.Forms.ListBox();
+            this.listBoxLine2 = new System.Windows.Forms.ListBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.labelPoint = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(572, 474);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(590, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Первая линия";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(590, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "y = ";
+            // 
+            // textBoxX1
+            // 
+            this.textBoxX1.Location = new System.Drawing.Point(612, 31);
+            this.textBoxX1.Name = "textBoxX1";
+            this.textBoxX1.Size = new System.Drawing.Size(24, 20);
+            this.textBoxX1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(637, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "x + ";
+            // 
+            // textBoxFreeCoef1
+            // 
+            this.textBoxFreeCoef1.Location = new System.Drawing.Point(660, 31);
+            this.textBoxFreeCoef1.Name = "textBoxFreeCoef1";
+            this.textBoxFreeCoef1.Size = new System.Drawing.Size(24, 20);
+            this.textBoxFreeCoef1.TabIndex = 5;
+            // 
+            // textBoxFreeCoef2
+            // 
+            this.textBoxFreeCoef2.Location = new System.Drawing.Point(794, 31);
+            this.textBoxFreeCoef2.Name = "textBoxFreeCoef2";
+            this.textBoxFreeCoef2.Size = new System.Drawing.Size(24, 20);
+            this.textBoxFreeCoef2.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(771, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "x + ";
+            // 
+            // textBoxX2
+            // 
+            this.textBoxX2.Location = new System.Drawing.Point(746, 31);
+            this.textBoxX2.Name = "textBoxX2";
+            this.textBoxX2.Size = new System.Drawing.Size(24, 20);
+            this.textBoxX2.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(724, 34);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 15);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "y = ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(724, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(76, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Вторая линия";
+            // 
+            // buttonDraw
+            // 
+            this.buttonDraw.Location = new System.Drawing.Point(593, 229);
+            this.buttonDraw.Name = "buttonDraw";
+            this.buttonDraw.Size = new System.Drawing.Size(75, 23);
+            this.buttonDraw.TabIndex = 11;
+            this.buttonDraw.Text = "Посчитать";
+            this.buttonDraw.UseVisualStyleBackColor = true;
+            this.buttonDraw.Click += new System.EventHandler(this.buttonDraw_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(593, 258);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Отобразить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBoxLine1
+            // 
+            this.listBoxLine1.FormattingEnabled = true;
+            this.listBoxLine1.Location = new System.Drawing.Point(593, 67);
+            this.listBoxLine1.Name = "listBoxLine1";
+            this.listBoxLine1.Size = new System.Drawing.Size(117, 134);
+            this.listBoxLine1.TabIndex = 13;
+            // 
+            // listBoxLine2
+            // 
+            this.listBoxLine2.FormattingEnabled = true;
+            this.listBoxLine2.Location = new System.Drawing.Point(727, 67);
+            this.listBoxLine2.Name = "listBoxLine2";
+            this.listBoxLine2.Size = new System.Drawing.Size(117, 134);
+            this.listBoxLine2.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(590, 302);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(105, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Точка пересечения";
+            // 
+            // labelPoint
+            // 
+            this.labelPoint.AutoSize = true;
+            this.labelPoint.Location = new System.Drawing.Point(701, 302);
+            this.labelPoint.Name = "labelPoint";
+            this.labelPoint.Size = new System.Drawing.Size(31, 13);
+            this.labelPoint.TabIndex = 16;
+            this.labelPoint.Text = "none";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(701, 331);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(31, 13);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "none";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(590, 331);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(100, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Угол пересечения";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(862, 497);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.labelPoint);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.listBoxLine2);
+            this.Controls.Add(this.listBoxLine1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonDraw);
+            this.Controls.Add(this.textBoxFreeCoef2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxX2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxFreeCoef1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxX1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxX1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxFreeCoef1;
+        private System.Windows.Forms.TextBox textBoxFreeCoef2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxX2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button buttonDraw;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBoxLine1;
+        private System.Windows.Forms.ListBox listBoxLine2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label labelPoint;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Practise/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Practise
+{
+    public partial class Form1 : Form
+    {
+        private float x1 = 0, x2 = 0;
+        private float freeCoef1 = 0, freeCoef2 = 0;
+
+        List<PointF> list1 = new List<PointF>();
+        List<PointF> list2 = new List<PointF>();
+        PointF crossPoint;
+
+        Bitmap bmp;
+        Graphics graph;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonDraw_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                x1 = Convert.ToSingle(textBoxX1.Text) * -1;
+                x2 = Convert.ToSingle(textBoxX2.Text) * -1;
+                freeCoef1 = Convert.ToSingle(textBoxFreeCoef1.Text);
+                freeCoef2 = Convert.ToSingle(textBoxFreeCoef2.Text);
+
+                list1.Clear();
+                list2.Clear();
+
+                for (float i = -100; i < 100; i += 10)
+                {
+                    PointF point = new PointF();
+                    point.X = i;
+                    point.Y = (x1 * i) + freeCoef1;
+                    point.X += 240;
+                    point.Y += 140;
+
+                    list1.Add(point);
+
+                    point.Y = (x2 * i) + freeCoef2;
+                    point.Y += 140;
+
+                    list2.Add(point);
+                }
+
+                listBoxLine1.DataSource = null;
+                listBoxLine2.DataSource = null;
+
+                listBoxLine1.DataSource = list1;
+                listBoxLine2.DataSource = list2;
+            }
+            catch(FormatException)
+            {
+                MessageBox.Show("Неккоректные данные");
+            }
+            catch (OverflowException)
+            {
+                MessageBox.Show("Неккоректные данные");
+            }
+
+            labelPoint.Text = findCross(x1, freeCoef1, x2, freeCoef2).ToString();
+            label8.Text = findAngle().ToString();
+
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            graph = Graphics.FromImage(bmp);
+            Pen pen = new Pen(Color.Black);
+            graph.DrawLine(pen, list1[0], list1[19]);
+            pen.Color = Color.Red;
+            graph.DrawLine(pen, list2[0], list2[19]);
+            pictureBox1.Image = bmp;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private PointF findCross(float x1, float freeCoef1, float x2, float freeCoef2)
+        {
+            float y1 = -1;
+            float y2 = -1;
+            x1 = -x1;
+            x2 = -x2;
+
+            PointF pt = new PointF();
+            float d = (float)(x1 * y2 - y1 * x2);
+            float dx = (float)(-freeCoef1 * y2 + y1 * freeCoef2);
+            float dy = (float)(-x1 * freeCoef2 + freeCoef1 * x2);
+            pt.X = (float)(dx / d);
+            pt.Y = (float)(dy / d);
+            return pt;
+
+            
+            
+        }
+       
+        private double findAngle()
+        {
+            float a1 = list1[0].X;
+            float a2 = list1[1].Y;
+            float b1 = list2[0].X;
+            float b2 = list2[1].Y;
+
+            float scalar = a1 * b1 + a2 * b2;
+            double modulA = Math.Sqrt(Math.Pow(a1, 2) + Math.Pow(a2, 2));
+            double modulB = Math.Sqrt(Math.Pow(b1, 2) + Math.Pow(b2, 2));
+            double cos = scalar / (modulA * modulB);
+
+            double angle = Math.Acos(cos);
+            return  Math.Round(angle * 100, 2);
+        }
+
+        https://github.com/RaskhozhevaNatalia/practika.git
+    }
+}
